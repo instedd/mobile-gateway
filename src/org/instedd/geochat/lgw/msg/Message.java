197ce7e4ebef913b49ed0fa061e4ac4f58f2f06d
aborenszweig <aborenszweig@manas.com.ar>
2010-11-10 21:37:10 -0300

    Started implementing qst client

---
+++ src/org/instedd/geochat/lgw/msg/Message.java
@@ -0,0 +1,10 @@
+package org.instedd.geochat.lgw.msg;
+
+public class Message {
+	
+	public String id;
+	public String from;
+	public String to;
+	public long when;
+
+}
--- src/org/instedd/geochat/lgw/msg/QstClient.java
+++ src/org/instedd/geochat/lgw/msg/QstClient.java
@@ -1,19 +1,49 @@
 package org.instedd.geochat.lgw.msg;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.http.HttpResponse;
 
 public class QstClient {
-
-	private final String name;
-	private final String password;
-
+	
+	private final IRestClient client;
+	
 	public QstClient(String name, String password) {
-		this.name = name;
-		this.password = password;
+		this(name, password, new RestClient());
 	}
 
-	public void sendAddress(String number) throws IOException {
+	public QstClient(String name, String password, IRestClient restClient) {
+		this.client = restClient;
+		this.client.setAuth(name, password);
+	}
+
+	public void sendAddress(String address) throws QstClientException {
+		try {
+			HttpResponse response = this.client.get("https://nuntium.instedd.org/instedd/qst/setaddress?address=" + encode(address));
+			if (response == null) throw new QstClientException("Status not HTTP_OK (200) on credentialsAreValid");
+			
+			response.getEntity().getContent().close();
+		} catch (IOException e) {
+			throw new QstClientException(e);
+		}	
+	}
+	
+	public void sendMessages(Message[] messages) throws IOException {
 		
+	}
+	
+	public Message[] getMessages() throws IOException {
+		return null;
+	}
+	
+	private String encode(String str) {
+		try {
+			return URLEncoder.encode(str, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return str;
+		}
 	}
 
 }
