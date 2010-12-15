f5f668621142144781716229c7c72b9b29443840
Ary Borenszweig <asterite@gmail.com>
2010-12-15 11:23:27 -0300

    Better handling of errors when sending phone messages. Added tries count to MTs

--- org.instedd.geochat.lgw/src/org/instedd/geochat/lgw/msg/Message.java
+++ org.instedd.geochat.lgw/src/org/instedd/geochat/lgw/msg/Message.java
@@ -13,14 +13,20 @@ public class Message {
 	public String to;
 	public String text;
 	public long when;
+	public int tries;
 	
 	public static Message readFrom(Cursor c) {
+		// Check the projections in GeoChatLgw
 		Message msg = new Message();
 		msg.guid = c.getString(1);
 		msg.from = c.getString(2);
 		msg.to = c.getString(3);
 		msg.text = c.getString(4);
 		msg.when = c.getLong(5);
+		// msg.sending = c.getInt(6);
+		if (c.getColumnCount() > 7) {
+			msg.tries = c.getInt(7);
+		}
 		return msg;
 	}
 	
