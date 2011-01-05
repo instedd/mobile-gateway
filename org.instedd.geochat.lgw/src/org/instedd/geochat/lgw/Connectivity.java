47b274b67f7d6bd641c67470a84c2dac1cb66bf7
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-01-05 12:43:47 -0300

    Try to re-enable connectivity when there is no connectivity

--- org.instedd.geochat.lgw/src/org/instedd/geochat/lgw/Connectivity.java
+++ org.instedd.geochat.lgw/src/org/instedd/geochat/lgw/Connectivity.java
@@ -3,6 +3,7 @@ package org.instedd.geochat.lgw;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 
 public class Connectivity {
 	
@@ -10,6 +11,12 @@ public class Connectivity {
 		ConnectivityManager conn = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo info = conn.getActiveNetworkInfo();
 		return info != null && info.isConnected();
+	}
+	
+	public static void reEnableWifi(Context context) {
+		WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		wifi.setWifiEnabled(false);
+		wifi.setWifiEnabled(true);
 	}
 
 }
